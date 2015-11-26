ac1140c061e79ed9895e26565639769d3af5b5cb
Louis Popi <theh2o64@gmail.com>
2015-11-26 17:41:15 +0100

    tomato: Update dalvik heap makefiles

--- device.mk
+++ device.mk
@@ -15,8 +15,7 @@
 #
 $(call inherit-product-if-exists, vendor/yu/tomato/tomato-vendor.mk)
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # WCNSS
 PRODUCT_COPY_FILES += \
