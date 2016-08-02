4fb30b3f4810ce31bada82f4e6a5c46964e1a3d5
Louis Popi <theh2o64@gmail.com>
2016-08-02 10:49:13 +0200

    tomato: Switch to QC releasetools

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,11 +98,13 @@ BOARD_USES_QCOM_HARDWARE := false
 TARGET_RECOVERY_FSTAB := device/yu/tomato/ramdisk/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
 TARGET_RECOVERY_DENSITY := xhdpi
+TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/yu/tomato
+TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
--- radio.mk
+++ radio.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #----------------------------------------------------------------------
-# Radio image
+# Firmware images
 #----------------------------------------------------------------------
 ifeq ($(ADD_RADIO_FILES), true)
 radio_dir := $(LOCAL_PATH)/radio
@@ -27,13 +27,3 @@ RADIO_FILES := $(shell cd $(radio_dir) ; ls)
 $(foreach f, $(RADIO_FILES), \
     $(call add-radio-file,radio/$(f)))
 endif
-
-TARGET_BOOTLOADER_EMMC_INTERNAL := $(LOCAL_PATH)/images/emmc_appsboot.mbn
-$(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
-
-INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
-$(call add-radio-file,images/hyp.mbn)
-$(call add-radio-file,images/NON-HLOS.bin)
-$(call add-radio-file,images/rpm.mbn)
-$(call add-radio-file,images/sbl1.mbn)
-$(call add-radio-file,images/tz.mbn)
