bcdee541b45c74c777d7f880570645d2cb594e33
Louis Popi <theh2o64@gmail.com>
2016-08-22 21:41:33 +0200

    tomato: Add Dalvik/HWUI properties

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/aosparadox/bootanimation/720p_PNG_bootanimation.zip:system/media/bootanimation.zip
 
+# Dalvik/HWUI
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # CodeAurora msm8916_64 Tree
 include device/qcom/msm8916_64/msm8916_64.mk
 
