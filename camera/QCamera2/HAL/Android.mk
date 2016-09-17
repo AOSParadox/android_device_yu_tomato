cf2adfb8002ba38d20d0ba08dd287b998e673db5
Louis Popi <theh2o64@gmail.com>
2016-09-17 15:34:12 +0200

    tomato: Upgrade camera HAL from LA.BR.1.2.6_rb1.17

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -78,7 +78,7 @@ endif
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 
 LOCAL_MODULE_RELATIVE_PATH    := hw
-LOCAL_MODULE := camera.msm8916
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
 
