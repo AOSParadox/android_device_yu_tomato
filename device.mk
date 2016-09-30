07deb8359b56580b4bb3489f423e1a3a8f068585
Louis Popi <theh2o64@gmail.com>
2016-09-30 20:39:01 +0200

    tomato: Upgrade to LA.BR.1.2.7.c1-06000-8x16.0

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_COPY_FILES += \
 
 # CAF Branch
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.par.branch=LA.BR.1.2.7.c1-05400-8x16.0
+    ro.par.branch= LA.BR.1.2.7.c1-06000-8x16.0
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
