b5eb646d3bb092245c39a9305826d8a9b5d84786
luca020400 <luca.stefani.ge1@gmail.com>
2016-10-26 09:43:18 +0200

    tomato dt: Move to in-build directory search
    * We can't depend on a $OUT variable with ninja

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -9,11 +9,18 @@ KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 else
 KERNEL_ARCH := $(TARGET_ARCH)
 endif
+possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
 
 # Build DT Image
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
+	$(call pretty,"Target dt image: $@")
+	$(hide) for dir in $(possible_dtb_dirs); do
+		if [ -d "$$dir" ]; then
+			dtb_dir="$$dir";
+			break;
+		fi;
+	done;
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ "$$dtb_dir"
 	@echo "Made DT image: $@"
 
 
