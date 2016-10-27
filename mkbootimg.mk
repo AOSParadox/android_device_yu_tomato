1044dc3bdc53cc8f5032cd4aa63631b31bbccad0
Louis Popi <theh2o64@gmail.com>
2016-10-27 20:33:01 +0200

    tomato: Hardcode everything
    * Haters gonna hate

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -13,7 +13,7 @@ endif
 # Build DT Image
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p out/target/product/tomato/obj/KERNEL_OBJ/scripts/dtc/ out/target/product/tomato/obj/KERNEL_OBJ/arch/arm64/boot/dts/
 	@echo "Made DT image: $@"
 
 
