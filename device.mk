72f2080afee1e08b44e7fd797e89c9875c310d4b
Louis Popi <theh2o64@gmail.com>
2016-11-27 17:48:18 +0100

    tomato: Switch to inherit for BSP camera

--- device.mk
+++ device.mk
@@ -14,8 +14,9 @@
 # limitations under the License.
 #
 $(call inherit-product-if-exists, vendor/yu/tomato/tomato-vendor.mk)
-$(call inherit-product-if-exists, vendor/volte/volte.mk)
+$(call inherit-product-if-exists, vendor/camera/camera.mk)
 $(call inherit-product-if-exists, vendor/volte/tomato/tomato-vendor.mk)
+$(call inherit-product-if-exists, vendor/volte/volte.mk)
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
@@ -38,7 +39,6 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk
 
 # CodeAurora msm8916_64 Tree
 include device/qcom/msm8916_64/msm8916_64.mk
-include vendor/camera/camera.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/yu/tomato/overlay
