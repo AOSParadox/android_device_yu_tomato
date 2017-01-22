33a6498b436012539fbefd76c799a2b9e57c9587
Louis Popi <theh2o64@gmail.com>
2017-01-22 07:55:01 +0100

    tomato: Import liblight from hardware/cyanogen

--- lights/Android.mk
+++ lights/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# We currently have customized supportfor AW2013 and QPNP controllers
+ifeq ($(call match-word-in-list,$(BOARD_LIGHTS_VARIANT),aw2013 qpnp),true)
+
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := lights.c
+LOCAL_SRC_FILES := lights-$(BOARD_LIGHTS_VARIANT).c
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 
-LOCAL_MODULE := lights.msm8916
+LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
